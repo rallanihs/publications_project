294b8dddd54147aa4d154fb43a906aff0011081d
rallanihs <rallan@ihs.gmu.edu>
2025-08-31 11:34:29 -0400

    Update main.py
    Added set binary_location to selenium functions

--- main.py
+++ main.py
@@ -160,6 +160,7 @@ async def download_wiley_selenium_async(pdfdirect_url: str, filepath: str):
         options.add_argument(f"--user-data-dir={temp_profile_dir}")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.binary_location = "/usr/bin/google-chrome-stable"
         
         driver = uc.Chrome(options=options)
 
@@ -242,6 +243,7 @@ async def elsevier_selenium_download_async(doi_url: str, filepath: str):
         chrome_options.add_experimental_option("prefs", {
             "profile.managed_default_content_settings.images": 2  # disable images
         })
+        options.binary_location = "/usr/bin/google-chrome-stable"
 
         driver = uc.Chrome(options=chrome_options)
         wait = WebDriverWait(driver, 15)
@@ -621,6 +623,7 @@ async def universal_download(url: str, filepath: str):
                 "plugins.always_open_pdf_externally": True,
             }
             chrome_options.add_experimental_option("prefs", prefs)
+            options.binary_location = "/usr/bin/google-chrome-stable"
 
             driver = webdriver.Chrome(options=chrome_options)
             driver.get(url)
